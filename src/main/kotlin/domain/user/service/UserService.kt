fc258c9c2b241d3bfed39b1a8ef6b9c879bcaabe
Jhzlo <kjh0010703@naver.com>
2025-02-09 00:47:13 +0900

    feat(user): login service 로직 구현

--- src/main/kotlin/domain/user/service/UserService.kt
+++ src/main/kotlin/domain/user/service/UserService.kt
@@ -2,6 +2,7 @@ package org.example.domain.user.service
 
 import org.example.domain.member.entity.User
 import org.example.domain.member.repository.UserRepository
+import org.example.domain.user.dto.LoginRequest
 import org.example.domain.user.dto.RegisterRequest
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -16,5 +17,10 @@ class UserService(
             password = passwordEncoder.encode(request.password)
         }
         userRepository.save(user)
+    }
+
+    fun login(email: String, rawPassword: String):User? {
+        val user = userRepository.findByEmail(email) ?: return null
+        return if (passwordEncoder.matches(rawPassword, user.password)) user else null
     }
 }
