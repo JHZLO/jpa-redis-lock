07cfcd506929805ac9f8f6c9c1d843f4631acaf9
Jhzlo <kjh0010703@naver.com>
2025-02-08 20:36:23 +0900

    refactor(member): column 수정

---
+++ src/main/kotlin/domain/member/controller/MemberApiController.kt
@@ -0,0 +1,4 @@
+package org.example.domain.member.controller
+
+class MemberApiController {
+}
--- src/main/kotlin/domain/member/entity/Member.kt
+++ src/main/kotlin/domain/member/entity/Member.kt
@@ -1,21 +1,20 @@
 package org.example.domain.member.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.example.domain.coupon.entity.Coupon
 
 @Entity
 @Table(name = "member")
 class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
 
-    @Column(name = "name")
-    var name: String? = null
+    @Column(name = "email", unique = true)
+    val email: String = ""
+
+    @Column(name = "password")
+    var password: String = ""
 
     @OneToOne
     @JoinColumn(name = "coupon_id", referencedColumnName = "id")
