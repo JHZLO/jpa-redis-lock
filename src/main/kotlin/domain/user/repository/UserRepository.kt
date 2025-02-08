4bf485e6a4c9cb183b5f682db590e73ba9c77a44
Jhzlo <kjh0010703@naver.com>
2025-02-09 01:06:44 +0900

    feat(user): login, register api 생성

--- src/main/kotlin/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/domain/user/repository/UserRepository.kt
@@ -2,8 +2,9 @@ package org.example.domain.member.repository
 
 import org.example.domain.member.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmail(email: String): User?
 }
--- src/main/kotlin/domain/user/service/UserService.kt
+++ src/main/kotlin/domain/user/service/UserService.kt
@@ -2,24 +2,25 @@ package org.example.domain.user.service
 
 import org.example.domain.member.entity.User
 import org.example.domain.member.repository.UserRepository
-import org.example.domain.user.dto.LoginRequest
 import org.example.domain.user.dto.RegisterRequest
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
 
+@Service
 class UserService(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder
 ) {
-    fun register(request: RegisterRequest){
+    fun register(request: RegisterRequest): User {
         val user = User().apply {
             email = request.email
             password = passwordEncoder.encode(request.password)
         }
         userRepository.save(user)
+        return user
     }
 
-    fun login(email: String, rawPassword: String):User? {
+    fun login(email: String, rawPassword: String): User? {
         val user = userRepository.findByEmail(email) ?: return null
         return if (passwordEncoder.matches(rawPassword, user.password)) user else null
     }
