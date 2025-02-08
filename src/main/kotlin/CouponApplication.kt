e8cbf76cb59767a2e3cc342b59c6c4acce3ba7f0
Jhzlo <kjh0010703@naver.com>
2025-02-08 17:26:37 +0900

    feat(application): application setting

---
+++ src/main/kotlin/CouponApplication.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class CouponApplication
+fun main(args:Array<String>) {
+    runApplication<CouponApplication>(*args)
+}
