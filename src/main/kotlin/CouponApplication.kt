7568064e1d67890d2d282be392a8349adf5cbaa7
Jhzlo <kjh0010703@naver.com>
2025-02-09 14:18:33 +0900

    chore: redis dependencies setting

--- src/main/kotlin/CouponApplication.kt
+++ src/main/kotlin/CouponApplication.kt
@@ -2,8 +2,9 @@ package org.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
-
+@EnableCaching
 @SpringBootApplication
 class CouponApplication
 fun main(args:Array<String>) {
